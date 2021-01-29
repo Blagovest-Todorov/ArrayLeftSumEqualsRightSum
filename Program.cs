1b3157ea449ecb62574388d985bb2c432afb52b2
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-29 18:12:57 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace equalSumsInArrayLeftSumRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)             //1 2 3 3 
+        {
+            int[] numbersArr = Console.ReadLine()
+                                     .Split()
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+            bool isNumber = false;
+            for (int currIndex = 0; currIndex < numbersArr.Length; currIndex++)
+            {               
+                int leftSum = 0;
+                int rightSum = 0;
+               
+                
+                // Left for loop for the left sum 
+                for (int i = currIndex - 1; i >= 0; i--)
+                {
+                    leftSum += numbersArr[i];
+                }
+                //rightSum Right LOOP
+                for (int i = currIndex + 1; i < numbersArr.Length; i++)
+                {
+                    rightSum += numbersArr[i];
+                }
+                
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(currIndex);
+                    isNumber = true;
+                    break;
+                } 
+            }
+
+            if (isNumber==false)
+            {
+                Console.WriteLine("no");
+            }
+           
+
+        }
+    }
+}
